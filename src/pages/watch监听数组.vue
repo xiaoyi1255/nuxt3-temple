e4e5ca7520166a03d8e4c8fea5e40a9dbede8987
xiaoyi <jmt1255@163.com>
2023-03-17 22:55:32 +0800

    feat: watch 和watchEffect案例使用

---
+++ src/pages/watch监听数组.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1 v-for="item in state.frond" :key="item">{{ item }}</h1>
+</template>
+
+<script setup lang="ts">
+const state = reactive({
+    frond: ['HTML', 'CSS', 'JavaScript'],
+    str: 0
+})
+watch(()=> state.frond, (val, oldVal) => {
+    console.log('watch 监听：', {val, oldVal})
+}, {
+    deep: false
+})
+let index = 0
+setInterval(() => {
+    state.frond.push('Nuxt3' + index++)
+    state.str = index
+}, 1000)
+
+watchEffect(() => {
+    console.log('watchEffect', state.str)
+
+})
+</script>
