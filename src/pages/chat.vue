18fa14e1a23558b93b60a51a2507a4fed4635270
静ming <1255274497@qq.com>
2023-08-15 21:24:47 +0800

    feat: chat add title

--- src/pages/chat.vue
+++ src/pages/chat.vue
@@ -3,3 +3,19 @@
       <Chat />
     </div>
   </template>
+
+<script setup>
+useHead({
+  titleTemplate: 'chat', // 可以写模板
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [
+    { name: 'description', content: 'chat' }
+  ],
+  // body添加类名
+  bodyAttrs: {
+    class: 'test'
+  }
+})
+
+</script>
