c53f3076200500619317c3d418ce05439910d893
静ming <1255274497@qq.com>
2023-08-21 23:38:05 +0800

    feat:  heic 类型

--- express/routes/upload.js
+++ express/routes/upload.js
@@ -3,16 +3,23 @@ const router = express.Router();
 const path = require('path');
 const Busboy = require('busboy')
 const fs = require('fs')
+const heicConvert = require('heic-convert');
 
 router.post('/imgs', (req, res) => {
   const busboy = Busboy({ headers: req.headers });
   let _fileName = ''
-  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+  busboy.on('file', async(fieldname, file, filename, encoding, mimetype) => {
     const names = filename.filename.split('.')
     const preName = names[0] + '-';
     _fileName = new Date().getTime() + '.' + 'png'
+    const arrayBuffer = await file.arrayBuffer();
+      const pngBuffer = await heicConvert({
+        buffer: arrayBuffer,
+        format: 'PNG',
+      });
+
     const saveTo = path.join(__dirname, '../public/uploads/', _fileName);
-    file.pipe(fs.createWriteStream(saveTo));
+    file.pipe(fs.createWriteStream(saveTo, pngBuffer));
   });
 
   busboy.on('finish', function () {
