8ba18b844d8d22ee47abdd200c47a834b80c12d2
静ming <jming.tai@backgardon.com>
2023-08-24 17:49:45 +0800

    pm2: node 多跑实例

--- pm2.config.js
+++ pm2.config.js
@@ -4,11 +4,13 @@ module.exports = {
         name: 'xiaoyiApp',
         port: '80',
         exec_mode: 'cluster',
+        instances: 2,
         script: './.output/server/index.mjs'
       },
       {
         name: 'ws',
         port: '3000',
+        instances: 7,
         exec_mode: 'cluster',
         script: './express/server.js'
       }
