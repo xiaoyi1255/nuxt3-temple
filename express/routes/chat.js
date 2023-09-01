f8bcb185b15df3b7bb2673751ca4cca9016d280b
静ming <jming.tai@backgardon.com>
2023-09-01 10:25:48 +0800

    genxin

--- express/routes/chat.js
+++ express/routes/chat.js
@@ -2,7 +2,7 @@
 const OpenAIApi  = require("openai");
 
 const openai = new OpenAIApi({
-	apiKey: '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'
+	apiKey: ''
 });
 async function main() {
 	const completion = await openai.chat.completions.create({
