217531d16cc8f03f9c158c195bdb77e4985bca69
静ming <1255274497@qq.com>
2023-09-17 22:51:27 +0800

    feat: 服务器port修改

--- express/pm2.config.js
+++ express/pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'ws',
-        port: '3000',
+        port: '3001',
         instances: 1,
         exec_mode: 'cluster',
         script: '../express/server.js'
