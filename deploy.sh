c63bace4e993356c1844a9a5a6f1f1d2b58a542a
静ming <1255274497@qq.com>
2023-09-19 22:31:52 +0800

    feat: 自动部署

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,15 @@
+echo "先拉一下代码"
+git pull
 cd client
+yarn 
+yarn generate
+rm -rf /www/wwwroot/chat-client/*
+cp ./dist/* /www/wwwroot/chat-client/ 
+echo "chat ui 复制完毕"
+
+cd ../express
+yarn
+yarn stop
 yarn deploy
+echo "后端项目跑起来了"
+
